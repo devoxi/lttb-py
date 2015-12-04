721c37ae5e8440edf512bb8fe5f1dcc1d9b589d8
Colin Hunt <colin.hunt@live.com>
2015-12-04 10:45:52 -0700

    Updating to work with Python 2.x. Formatting improvements. Custom exception class and improved error messages. Data can be list of tuples now.

--- lttb/lttb.py
+++ lttb/lttb.py
@@ -1,4 +1,4 @@
-'''
+"""
 The MIT License (MIT)
 
 Copyright (c) 2015 Olivier Devoisin
@@ -20,38 +20,44 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-'''
+"""
 
 import math
 
 
+class LttbException(Exception):
+    pass
+
+
 def largest_triangle_three_buckets(data, threshold):
     """
+    https://github.com/devoxi/lttb-py/blob/master/lttb/lttb.py
     Return a downsampled version of data.
-
     Parameters
     ----------
-    data: list of lists
+    data: list of lists/tuples
         data must be formated this way: [[x,y], [x,y], [x,y], ...]
+                                    or: [(x,y), (x,y), (x,y), ...]
     threshold: int
         threshold must be >= 2 and <= to the len of data
-
     Returns
     -------
     data, but downsampled using threshold
+
+    (This copy modified to work on Python 2.5)
     """
 
     # Check if data and threshold are valid
     if not isinstance(data, list):
-        raise Exception("incorrect data provided")
+        raise LttbException("data is not a list")
     if not isinstance(threshold, int) or threshold <= 2 or threshold >= len(data):
-        raise Exception("threshold not well defined")
+        raise LttbException("threshold not well defined")
     for i in data:
-        if not isinstance(i, list) or len(i) != 2:
-            raise Exception("incorrect data provided")
+        if not isinstance(i, (list, tuple)) or len(i) != 2:
+            raise LttbException("datapoints are not lists or tuples")
 
     # Bucket size. Leave room for start and end data points
-    every = (len(data) - 2)/(threshold - 2)
+    every = (len(data) - 2) / (threshold - 2)
 
     a = 0  # Initially a is the first point in the triangle
     next_a = 0
@@ -59,12 +65,12 @@ def largest_triangle_three_buckets(data, threshold):
 
     sampled = [data[0]]  # Always add the first point
 
-    for i in range(0, threshold-2):
+    for i in range(0, threshold - 2):
         # Calculate point average for next bucket (containing c)
         avg_x = 0
         avg_y = 0
-        avg_range_start = math.floor((i+1)*every) + 1
-        avg_range_end = math.floor((i+2)*every) + 1
+        avg_range_start = int(math.floor((i + 1) * every) + 1)
+        avg_range_end = int(math.floor((i + 2) * every) + 1)
         avg_rang_end = avg_range_end if avg_range_end < len(data) else len(data)
 
         avg_range_length = avg_rang_end - avg_range_start
@@ -78,8 +84,8 @@ def largest_triangle_three_buckets(data, threshold):
         avg_y /= avg_range_length
 
         # Get the range for this bucket
-        range_offs = math.floor((i+0)*every) + 1
-        range_to = math.floor((i+1)*every) + 1
+        range_offs = int(math.floor((i + 0) * every) + 1)
+        range_to = int(math.floor((i + 1) * every) + 1)
 
         # Point a
         point_ax = data[a][0]
@@ -89,7 +95,12 @@ def largest_triangle_three_buckets(data, threshold):
 
         while range_offs < range_to:
             # Calculate triangle area over three buckets
-            area = math.fabs((point_ax - avg_x)*(data[range_offs][1] - point_ay) - (point_ax - data[range_offs][0])*(avg_y-point_ay))*0.5
+            area = math.fabs(
+                (point_ax - avg_x)
+                * (data[range_offs][1] - point_ay)
+                - (point_ax - data[range_offs][0])
+                * (avg_y - point_ay)
+            ) * 0.5
             if area > max_area:
                 max_area = area
                 max_area_point = data[range_offs]
@@ -99,6 +110,6 @@ def largest_triangle_three_buckets(data, threshold):
         sampled.append(max_area_point)  # Pick this point from the bucket
         a = next_a  # This a is the next a (chosen b)
 
-    sampled.append(data[len(data)-1])  # Always add last
+    sampled.append(data[len(data) - 1])  # Always add last
 
     return sampled
